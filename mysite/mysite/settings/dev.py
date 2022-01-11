d363e1f524c2ce224342d93a7c56f027186540c2
Vitaly <ivanov.arduan@yandex.ru>
2022-01-11 17:08:32 +0300

    Мой первый опыт с wagtail.

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-$08-$d_ct59i@#kdl(y8-dv9i5f(xj4$tj*o3nz&!l#f0zy0rq'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
